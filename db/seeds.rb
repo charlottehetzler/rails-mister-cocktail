16780cfbf9d3024bf443de175e4a049442119d82
Charlotte Hetzler <charlotte@hetzler.de>
2019-06-19 17:21:11 +0300

    Mr Cocktail First Day

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+require 'open-uri'
+
+puts 'starting json'
+serialized_drink = open('https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list').read
+drinks = JSON.parse(serialized_drink)
+drinks['drinks'].each do |drink|
+  Ingredient.create!(name: drink["strIngredient1"])
+end
+puts '3 more'
+
+Ingredient.create(name: 'lemon')
+Ingredient.create(name: 'ice')
+Ingredient.create(name: 'mint leaves')
+
+puts 'done'
