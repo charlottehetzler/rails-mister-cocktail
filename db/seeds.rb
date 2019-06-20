d8d7b8754888d1d71138c058c65a9a0a2798aedf
Charlotte Hetzler <charlotte@hetzler.de>
2019-06-20 15:12:41 +0300

    still fixing

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,4 +16,44 @@ Ingredient.create(name: 'lemon')
 Ingredient.create(name: 'ice')
 Ingredient.create(name: 'mint leaves')
 
+Cocktail.create(name: 'Campari Orange', photo: '../app/assets/images/cocktail_CampariOrange.jpg')
+
 puts 'done'
+
+# require 'json'
+# require 'open-uri'
+
+# url = 'http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+# ingredients_serialized = open(url).read
+# ingredients = JSON.parse(ingredients_serialized)
+# cocktails = ["Gin", "Mojito", "Happy", "Whiskey", "Spritz", "Vodka", "Strawberry", "SuperGin"]
+
+# puts "seeding starts"
+
+# Cocktail.destroy_all
+# Ingredient.destroy_all
+
+# cocktails.each do |cocktail|
+#   drink = Cocktail.new(name: cocktail)
+
+#   if drink.save
+#     puts "#{drink.name} saved"
+#   end
+# end
+# # 5.times do
+# #   drink = Cocktail.new(name: Faker::BossaNova.song)
+
+# #   if drink.save
+# #     puts "#{drink.name} saved"
+# #   end
+# # end
+
+# ingredients["drinks"].each do |ingredient|
+#   ing = Ingredient.new(name: ingredient["strIngredient1"])
+
+#   if ing.save
+#     puts "#{ing.name} saved"
+#   end
+# end
+
+# puts "seeding finished"
